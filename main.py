97e764f2efbca4816fa5f4d1eec92132b69c6c1f
Brad Traversy <traversymedia@gmail.com>
2019-12-21 11:19:48 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import speech_recognition as sr
+import webbrowser
+import time
+import playsound
+import os
+import random
+from gtts import gTTS
+from time import ctime
+
+
+def record_audio(ask=False):
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        if ask:
+            alexis_speak(ask)
+            print(ask)
+        # Capture mic data
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            alexis_speak('Sorry, I did not get that')
+        except sr.RequestError:
+            alexis_speak('Sorry, my service is down')
+        return voice_data
+
+
+def alexis_speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en')
+    r = random.randint(1, 10000000)
+    audio_file = 'audio-' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    os.remove(audio_file)
+
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        alexis_speak('My name is Alexis')
+        print('My name is Alexis')
+    if 'what time is it' in voice_data:
+        alexis_speak(ctime())
+    if 'search' in voice_data:
+        search = record_audio('What do you want to search?')
+        url = 'https://www.google.com/search?q=' + search
+        webbrowser.get().open(url)
+        alexis_speak('Here is what I found for ' + search + '...')
+        print('Here is what I found for ' + search + '...')
+    if 'find location' in voice_data:
+        location = record_audio('What is the location?')
+        url = 'https://google.nl/maps/place/' + location + '/&amp;'
+        webbrowser.get().open(url)
+        alexis_speak('Here is the location for ' + location)
+        print('Here is the location for ' + location)
+    if 'exit' in voice_data:
+        exit()
+
+
+time.sleep(1)
+alexis_speak('How can I help you?')
+while 1:
+    voice_data = record_audio()
+    respond(voice_data)
