7db778f3f2054ec310f95e329c2fef95f87c8190
Reza N <32329310+rezan21@users.noreply.github.com>
2019-12-24 23:47:05 +0000

    extended features
    Alexis can now greet, remember user's name, search youtube and get stock prices

--- main.py
+++ main.py
@@ -1,62 +1,128 @@
-import speech_recognition as sr
-import webbrowser
-import time
-import os
+import speech_recognition as sr # recognise speech
+import playsound # to play an audio file
+from gtts import gTTS # google text to speech
 import random
-import pyttsx3
-from time import ctime
+from time import ctime # get time details
+import webbrowser # open browser
+import yfinance as yf # to fetch financial data
+import ssl
+import certifi
+import time
+import os # to remove created audio files
 
-r = sr.Recognizer()
+class person:
+    name = ''
+    def setName(self, name):
+        self.name = name
 
-speaker = pyttsx3.init('sapi5')
-voices = speaker.getProperty('voices')
-speaker.setProperty('voice', voices[1].id)
-rate = speaker.getProperty('rate')
-speaker.setProperty('rate', 173)
+def there_exists(terms):
+    for term in terms:
+        if term in voice_data:
+            return True
 
-
+r = sr.Recognizer() # initialise a recogniser
+# listen for audio and convert it to text:
 def record_audio(ask=False):
-    with sr.Microphone() as source:
+    with sr.Microphone() as source: # microphone as source
         if ask:
-            alexis_speak(ask)
-        audio = r.listen(source)
+            speak(ask)
+        audio = r.listen(source)  # listen for the audio via source
         voice_data = ''
         try:
-            voice_data = r.recognize_google(audio)
-        except sr.UnknownValueError:
-            alexis_speak('Sorry, I did not get that')
+            voice_data = r.recognize_google(audio)  # convert audio to text
+        except sr.UnknownValueError: # error: recognizer does not understand
+            speak('I did not get that')
         except sr.RequestError:
-            alexis_speak('Sorry, my speech service is down')
-        return voice_data
+            speak('Sorry, the service is down') # error: recognizer is not connected
+        print(f">> {voice_data.lower()}") # print what user said
+        return voice_data.lower()
 
-
-def alexis_speak(audio_string):
-    print('Computer: ' + audio_string)
-    speaker.say(audio_string)
-    speaker.runAndWait()
-
+# get string and make a audio file to be played
+def speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en') # text to speech(voice)
+    r = random.randint(1,20000000)
+    audio_file = 'audio' + str(r) + '.mp3'
+    tts.save(audio_file) # save as mp3
+    playsound.playsound(audio_file) # play the audio file
+    print(f"kiri: {audio_string}") # print what app said
+    os.remove(audio_file) # remove audio file
 
 def respond(voice_data):
-    if 'what is your name' in voice_data:
-        alexis_speak('My name is Alexis')
-    if 'what time is it' in voice_data:
-        alexis_speak(ctime())
-    if 'search' in voice_data:
-        search = record_audio('What do you want to search for?')
-        url = 'https://google.com/search?q=' + search
+    # 1: greeting
+    if there_exists(['hey','hi','hello']):
+        greetings = [f"hey, how can I help you {person_obj.name}", f"hey, what's up? {person_obj.name}", f"I'm listening {person_obj.name}", f"how can I help you? {person_obj.name}", f"hello {person_obj.name}"]
+        greet = greetings[random.randint(0,len(greetings)-1)]
+        speak(greet)
+
+    # 2: name
+    if there_exists(["what is your name","what's your name","tell me your name"]):
+        if person_obj.name:
+            speak("my name is Alexis")
+        else:
+            speak("my name is Alexis. what's your name?")
+
+    if there_exists(["my name is"]):
+        person_name = voice_data.split("is")[-1].strip()
+        speak(f"okay, i will remember that {person_name}")
+        person_obj.setName(person_name) # remember name in person object
+
+    # 3: greeting
+    if there_exists(["how are you","how are you doing"]):
+        speak(f"I'm very well, thanks for asking {person_obj.name}")
+
+    # 4: time
+    if there_exists(["what's the time","tell me the time","what time is it"]):
+        time = ctime().split(" ")[3].split(":")[0:2]
+        if time[0] == "00":
+            hours = '12'
+        else:
+            hours = time[0]
+        minutes = time[1]
+        time = f'{hours} {minutes}'
+        speak(time)
+
+    # 5: search google
+    if there_exists(["search for"]) and 'youtube' not in voice_data:
+        search_term = voice_data.split("for")[-1]
+        url = f"https://google.com/search?q={search_term}"
         webbrowser.get().open(url)
-        alexis_speak('Here is what I found for ' + search)
-    if 'find location' in voice_data:
-        location = record_audio('What is the location?')
-        url = 'https://google.nl/maps/place/' + location + '/&amp;'
+        speak(f'Here is what I found for {search_term} on google')
+
+    # 6: search youtube
+    if there_exists(["youtube"]):
+        search_term = voice_data.split("for")[-1]
+        url = f"https://www.youtube.com/results?search_query={search_term}"
         webbrowser.get().open(url)
-        alexis_speak('Here is the location of ' + location)
-    if 'exit' in voice_data:
+        speak(f'Here is what I found for {search_term} on youtube')
+
+    # 7: get stock price
+    if there_exists(["price of"]):
+        search_term = voice_data.lower().split(" of ")[-1].strip() #strip removes whitespace after/before a term in string
+        stocks = {
+            "apple":"AAPL",
+            "microsoft":"MSFT",
+            "facebook":"FB",
+            "tesla":"TSLA",
+            "bitcoin":"BTC-USD"
+        }
+        try:
+            stock = stocks[search_term]
+            stock = yf.Ticker(stock)
+            price = stock.info["regularMarketPrice"]
+
+            speak(f'price of {search_term} is {price} {stock.info["currency"]} {person_obj.name}')
+        except:
+            speak('oops, something went wrong')
+    if there_exists(["exit", "quit", "goodbye"]):
+        speak("going offline")
         exit()
 
 
 time.sleep(1)
-alexis_speak('How can I help you?')
-while 1:
-    voice_data = record_audio()
-    respond(voice_data)
+
+person_obj = person()
+while(1):
+    voice_data = record_audio() # get the voice input
+    respond(voice_data) # respond
+
+
